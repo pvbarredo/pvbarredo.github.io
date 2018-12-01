4ae33caf1a91181a8b70e1d33c369a257739f0b2
pvbarredo <pvbarredo@gmail.com>
2018-12-02 00:26:20 +0800

    Updates

---
+++ precache-manifest.22f191643340aa9ae02b0fda9dc86ade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4512985089b01c7ce109",
+    "url": "/static/js/main.45129850.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "4512985089b01c7ce109",
+    "url": "/static/css/main.6bcbfe96.chunk.css"
+  },
+  {
+    "revision": "19d9bf099de197fac4406d7b8935f12c",
+    "url": "/index.html"
+  }
+];
