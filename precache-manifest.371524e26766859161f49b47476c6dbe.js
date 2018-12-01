8709755970a7753f1be24a4d23a16362339fe807
pvbarredo <pvbarredo@gmail.com>
2018-12-02 02:11:29 +0800

    Apply template change

---
+++ precache-manifest.371524e26766859161f49b47476c6dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f2da1476187611125fc5",
+    "url": "/static/js/main.f2da1476.chunk.js"
+  },
+  {
+    "revision": "33d6211426a5a82bb4ab",
+    "url": "/static/js/1.33d62114.chunk.js"
+  },
+  {
+    "revision": "f2da1476187611125fc5",
+    "url": "/static/css/main.96241688.chunk.css"
+  },
+  {
+    "revision": "8115ce33b2b01b244dae60a2b4738cb2",
+    "url": "/index.html"
+  }
+];
